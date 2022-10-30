79fb48864711b9338e726fd47304554cb9b47592
blackboardcoder <blackboardcoder@gmail.com>
2022-10-30 17:43:45 +0530

    Java

---
+++ 0. Hello World/Hello_World.java
@@ -0,0 +1,9 @@
+package Hello_World;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ 1. Data Types/Primitive_Types.java
@@ -0,0 +1,43 @@
+package Data_Types;
+
+public class Primitive_Types {
+
+	public static void main(String[] args) {
+		
+		//Integers Type
+		byte  b = 10;
+		short s = 20;
+		int   i = 30;
+		long  l = 123456;
+		
+		System.out.println("****Integers Type****");
+		System.out.println("Byte Type Value: " +b);
+		System.out.println("Short Type Value: " +s);
+		System.out.println("Integer Type Value: " +i);
+		System.out.println("Long Type Value: " +l);
+		
+		//Floating-Point Type
+		float  f = 1.23f; //f should be passed as suffix to the value
+		double d = 2.23456;
+		System.out.println("****Floating-Point Type****");
+		System.out.println("Float Type Value: " +f);
+		System.out.println("Double Type Value: " +d);
+		
+		//Character Type
+		char c = 'A';
+		System.out.println("****Character Type****");
+		System.out.println("Character Type Value: " +c);
+		
+		//Boolean Type
+		boolean bl =false;
+		System.out.println("****Boolean Type****");
+		System.out.println("Boolean Type Value: "+bl);
+	}
+
+}
+
+
+/*
+ * Author: Krishna
+ * 
+*/
