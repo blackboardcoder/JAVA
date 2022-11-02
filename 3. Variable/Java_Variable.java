b04ae4b5e2cdcd4771dcdddb55d045453449ea58
blackboardcoder <blackboardcoder@gmail.com>
2022-11-02 18:15:29 +0530

    Java

---
+++ 3. Variable/Java_Variable.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class Java_Variable {
+
+	public static void main(String[] args) {
+		//In Java, all variables must be declared before they can be used.
+		
+		int b; 		 //Declares b Variable
+		int v = 10;  //Declared and initialized v variable with 10 value
+		int c = v*5; //Dynamic Initialization
+		System.out.print("Value of c: " +c);
+	}
+
+}
